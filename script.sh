9be0dd6638817f1e11033e91a484d3377e370c0b
Muhammad Shakeeb <mshakeeb918@gmail.com>
2020-04-09 00:02:38 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+INPUT=$1
+INVENTORYFILEPATH=./inventory.yaml
+
+function checkifavailable () {
+if [[ $SERVER_IP = *.*.*.* ]]; then
+           echo "SSH to Server = $SERVER_IP"
+	   ssh ubuntu@"$SERVER_IP"
+   else
+           echo "Data for this server is not present in Inventorylist"
+   fi
+}
+
+function checkforserverip () {
+   SERVER_IP=$(cat < $INVENTORYFILEPATH | grep "ip: $INPUT" -A1 | grep bastion | awk '{print $2}')
+   checkifavailable
+}
+
+
+function checkforservername () {
+	SERVER_IP=$(cat < $INVENTORYFILEPATH | grep "$INPUT" -A2 | grep bastion | awk '{print $2}')
+   checkifavailable
+}
+
+if [[ $INPUT = *.*.*.* ]]; then
+	checkforserverip
+elif [[ $INPUT = server*  &&  ${#INPUT} -gt 6 ]]; then 
+	checkforservername
+else
+	echo "Please Enter correct ServerName or IP"
+
+fi
